67e63970b91e5cdfe0aafd383dc30da126473db3
Salonee Shrestha <saloneeshrestha35@gmail.com>
2024-02-10 19:41:03 +0545

    Resources.

--- script.js
+++ script.js
@@ -1,5 +1,54 @@
+//Game Constants and Variables
 let direction={x:0, y:0};
-let foodSound=new Audio('eating.mp3');
-let gameOverSound=new Audio('gameover.mp3');
-let moveSound=new Audio('move.mp3');
-let musicSound=new Audio('music.mp3');
+const foodSound=new Audio('eating.mp3');
+const gameOverSound=new Audio('gameover.mp3');
+const moveSound=new Audio('move.mp3');
+const musicSound=new Audio('music.mp3');
+let speed=2;
+let lastPaintTime=0;
+let snakeArr=[
+    {x:13, y:15}
+]
+food={x:6, y:7};
+
+//Game functions
+function main(ctime){
+    window.requestAnimationFrame(main);
+    console.log(ctime)
+    if((ctime-lastPaintTime)/1000 < 1/speed){
+        return;
+    }
+    lastPaintTime=ctime;
+    gameEngine();
+}
+
+function gameEngine(){
+    //Updating the snake array and food
+
+    //Display the snake
+    board.innerHTML="";
+    snakeArr.forEach((e, index)=>{
+        snakeElement=document.createElement('div');
+        snakeElement.style.gridRowStart=e.y;
+        snakeElement.style.gridColumnStart=e.x;
+        snakeElement.classlist.add('snake');
+        if(index===0){
+            snakeElement.classlist.add('head');
+        }
+        else{
+            snakeElement.classlist.add('snake');
+        }
+        board.appendChild(snakeElement);
+    });
+
+    //Display the food
+    foodElement=document.createElement('div');
+    foodElement.style.gridRowStart=food.y;
+    foodElement.style.gridColumnStart=food.x;
+    foodElement.classlist.add('food');
+    board.appendChild(foodElement);
+}
+
+
+//Main logic of game
+window.requestAnimationFrame(main);
