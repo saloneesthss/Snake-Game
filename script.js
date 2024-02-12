aa60dc060a6b6cb47f625fb096bb9d19526e4b3d
Salonee Shrestha <saloneeshrestha35@gmail.com>
2024-02-12 21:13:11 +0545

    Changes

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ food={x:6, y:7};
 //Game functions
 function main(ctime){
     window.requestAnimationFrame(main);
-    console.log(ctime)
+    //console.log(ctime)
     if((ctime-lastPaintTime)/1000 < 1/speed){
         return;
     }
@@ -57,19 +57,27 @@ window.addEventListener('keydown',e=>{
     moveSound.play();
     switch (e.key) {
         case "ArrowUp":
-            console.log("ArrowUp")
+            console.log("ArrowUp");
+            inputDir.x=0;
+            inputDir.y=-1;
             break;
 
         case "ArrowDown":
-                console.log("ArrowDown")
-                break;
+            console.log("ArrowDown");
+            inputDir.x=0;
+            inputDir.y=1;
+            break;
 
         case "ArrowLeft":
-            console.log("ArrowLeft")
+            console.log("ArrowLeft");
+            inputDir.x=-1;
+            inputDir.y=0;
             break;
 
         case "ArrowRight":
-            console.log("ArrowRight")
+            console.log("ArrowRight");
+            inputDir.x=1;
+            inputDir.y=0;
             break;
         
         default:
